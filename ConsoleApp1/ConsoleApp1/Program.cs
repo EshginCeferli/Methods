750ed31d84c70b0aecf8105b171662f3884a9da4
EshginCeferli <eshginij@code.edu.az>
2022-06-20 19:14:25 +0400

    Methods

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,130 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//1) 3 e ve 7 ye bolunen
+
+//Result();
+
+//static void Result()
+//{
+//    int n = 42;
+//    if (n % 21 == 0)
+//    {
+//        Console.WriteLine("Bolunur");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Bolunmur");
+//    }
+//}
+
+
+
+//2) Cutdurse cemini tapin
+
+//Sum();
+
+//static void Sum()
+//{
+//    int n = 3;
+//    int m = 8;
+//    int sum = 0;
+//    if (n % 2 == 0 && m % 2 == 0)
+//    {
+//        sum = n + m;
+//        Console.WriteLine(sum);
+//    }
+//    else
+//    {
+//        Console.WriteLine("Bolunmur");
+//    }
+//}   
+
+
+
+//3) n ve m arasindaki tek ededlerin cemin tapin
+
+//int sum = Sum();
+//Console.WriteLine(sum);
+
+//static int Sum()
+//{
+//    int n = 0;
+//    int m = 5;
+//    int sum = 0;
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 2 == 1)
+//            sum += i;
+//    }
+
+//    return sum;
+
+//}
+
+
+
+//4) arraydaki tek ededlerin cemini tapin
+
+//int sum = Array();
+//Console.WriteLine(sum);
+
+
+
+//static int Array()
+//{
+//    int[] tek = { 1, 2, 3, 4, 5, 6, 7, };
+//    int sum = 0;
+//    for (int i = 0; i < tek.Length; i++)
+//    {
+//        if (tek[i] % 2 == 1)
+//            sum += tek[i];
+
+
+//    }
+//    return sum;
+//}
+
+
+
+//5) arraydaki cut ededlerin sayini tapin 
+
+//Cut();
+
+//static void Cut()
+
+//{
+//    int[] cut = { 1, 2, 3, 4, 5, 6, 7, 8 };
+//    int count = 0;
+//    for (int i = 0; i < cut.Length; i++)
+//    {
+//        if (cut[i] % 2 == 0)
+//            count++;
+//    }
+//    Console.WriteLine(count);
+//}
+
+
+
+
+//6) murekkeb ve ya sade eded olmasini tapin
+
+//Method();
+//static void Method()
+//{
+//    int n = 13;
+//    int count = 0;
+//    for (int i = 2; i < n; i++)
+//    {
+//        if (n % i == 0)
+//            count++;
+//    }
+//    if (count == 2)
+//    {
+//        Console.WriteLine("Complex");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Simple");
+//    }
+//}
